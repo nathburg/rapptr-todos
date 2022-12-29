60e5c42e96d7f07de9b81aaf3a61433c9e15909e
Nathan Burgess <nathans.email.address@gmail.com>
2022-12-29 15:59:32 -0800

    PUT /todos/id edits description of todo

--- __tests__/todos.test.js
+++ __tests__/todos.test.js
@@ -1,53 +1,53 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
+const pool = require("../lib/utils/pool");
+const setup = require("../data/setup");
+const request = require("supertest");
+const app = require("../lib/app");
 
 const mockUser = {
-	email: 'kawhi.leonard@rapptr.com',
-	password: 'toronto123',
+  email: "kawhi.leonard@rapptr.com",
+  password: "toronto123",
 };
 
 const signInAndLogin = async (userProps = {}) => {
-	const password = userProps.password ?? mockUser.password;
-	const user = await request(app)
-		.post('/users')
-		.send({ ...mockUser, ...userProps });
-	const agent = request.agent(app);
-	await agent.post('/users/sessions').send({
-		...mockUser,
-		...userProps,
-	});
-	const { email } = user;
-	await agent.post('/users/sessions').send({ email, password });
+  const password = userProps.password ?? mockUser.password;
+  const user = await request(app)
+    .post("/users")
+    .send({ ...mockUser, ...userProps });
+  const agent = request.agent(app);
+  await agent.post("/users/sessions").send({
+    ...mockUser,
+    ...userProps,
+  });
+  const { email } = user;
+  await agent.post("/users/sessions").send({ email, password });
 
-	return agent;
+  return agent;
 };
 
-describe('todos routes', () => {
-	beforeEach(() => {
-		return setup(pool);
-	});
-	afterAll(() => {
-		pool.end();
-	});
+describe("todos routes", () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
 
-	it('only lets authenticated users see todo routes', async () => {
-		const resOne = await request(app).get('/todos');
-		expect(resOne.status).toBe(401);
-		const agent = await signInAndLogin();
-		const resTwo = await agent.get('/todos');
-		expect(resTwo.status).toBe(200);
-	});
+  it("only lets authenticated users see todo routes", async () => {
+    const resOne = await request(app).get("/todos");
+    expect(resOne.status).toBe(401);
+    const agent = await signInAndLogin();
+    const resTwo = await agent.get("/todos");
+    expect(resTwo.status).toBe(200);
+  });
 
-	it('posts new todo', async () => {
-		const agent = await signInAndLogin();
-		const resPost = await agent
-			.post('/todos')
-			.send({ description: 'Make New Years resolution.' });
-		expect(resPost.status).toBe(200);
-		const resGet = await agent.get('/todos');
-		expect(resGet.body).toMatchInlineSnapshot(`
+  it("posts new todo", async () => {
+    const agent = await signInAndLogin();
+    const resPost = await agent
+      .post("/todos")
+      .send({ description: "Make New Years resolution." });
+    expect(resPost.status).toBe(200);
+    const resGet = await agent.get("/todos");
+    expect(resGet.body).toMatchInlineSnapshot(`
       Array [
         Object {
           "description": "Make New Years resolution.",
@@ -57,13 +57,13 @@ describe('todos routes', () => {
         },
       ]
     `);
-	});
+  });
 
-	it("returns all user's uncompleted todos at GET /todos", async () => {
-		const agentOne = await signInAndLogin();
-		await agentOne.post('/todos').send({ description: 'Clean dishes' });
-		const resOne = await agentOne.get('/todos');
-		expect(resOne.body).toMatchInlineSnapshot(`
+  it("returns all user's uncompleted todos at GET /todos", async () => {
+    const agentOne = await signInAndLogin();
+    await agentOne.post("/todos").send({ description: "Clean dishes" });
+    const resOne = await agentOne.get("/todos");
+    expect(resOne.body).toMatchInlineSnapshot(`
       Array [
         Object {
           "description": "Clean dishes",
@@ -73,14 +73,14 @@ describe('todos routes', () => {
         },
       ]
     `);
-		await agentOne.delete('/users/sessions');
-		const agentTwo = await signInAndLogin({
-			email: 'v.lossy@rapptr.com',
-			password: 'password',
-		});
-		await agentTwo.post('/todos').send({ description: 'Eat raw meat.' });
-		const resTwo = await agentTwo.get('/todos');
-		expect(resTwo.body).toMatchInlineSnapshot(`
+    await agentOne.delete("/users/sessions");
+    const agentTwo = await signInAndLogin({
+      email: "v.lossy@rapptr.com",
+      password: "password",
+    });
+    await agentTwo.post("/todos").send({ description: "Eat raw meat." });
+    const resTwo = await agentTwo.get("/todos");
+    expect(resTwo.body).toMatchInlineSnapshot(`
       Array [
         Object {
           "description": "Eat raw meat.",
@@ -90,27 +90,44 @@ describe('todos routes', () => {
         },
       ]
     `);
-	});
+  });
 
-	it('returns the todo with given id at GET /todos/id, if it was created by the user', async () => {
-		const agentOne = await signInAndLogin();
-		await agentOne.post('/todos').send({ description: 'Clean dishes' });
-		const resOne = await agentOne.get('/todos/1');
-		expect(resOne.status).toBe(200);
-		await agentOne.delete('/users/sessions');
-		const agentTwo = await signInAndLogin({
-			email: 'v.lossy@rapptr.com',
-			password: 'password',
-		});
-		const resTwo = await agentTwo.get('/todos/1');
-		expect(resTwo.status).toBe(403);
-	});
+  it("returns the todo with given id at GET /todos/id, if it was created by the user", async () => {
+    const agentOne = await signInAndLogin();
+    await agentOne.post("/todos").send({ description: "Clean dishes" });
+    const resOne = await agentOne.get("/todos/1");
+    expect(resOne.status).toBe(200);
+    await agentOne.delete("/users/sessions");
+    const agentTwo = await signInAndLogin({
+      email: "v.lossy@rapptr.com",
+      password: "password",
+    });
+    const resTwo = await agentTwo.get("/todos/1");
+    expect(resTwo.status).toBe(403);
+  });
 
-	it('marks a todo complete (soft delete) at DELETE /todos/:id', async () => {
-		const agent = await signInAndLogin();
-		await agent.post('/todos').send({ description: 'Clean dishes' });
-		await agent.delete('/todos/1');
-		const res = await agent.get('/todos');
-		expect(res.body).toEqual({ message: 'You have no todos.' });
-	});
+  it("marks a todo complete (soft delete) at DELETE /todos/id", async () => {
+    const agent = await signInAndLogin();
+    await agent.post("/todos").send({ description: "Clean dishes" });
+    await agent.delete("/todos/1");
+    const res = await agent.get("/todos");
+    expect(res.body).toEqual({ message: "You have no todos." });
+  });
+
+  it("edits a todo's description at PUT /todos/id", async () => {
+    const agent = await signInAndLogin();
+    await agent.post("/todos").send({ description: "Clean dishes" });
+    await agent.put("/todos/1").send({ description: "Break dishes" });
+    const res = await agent.get("/todos");
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "description": "Break dishes",
+          "id": "1",
+          "isCompleted": false,
+          "userId": "1",
+        },
+      ]
+    `);
+  });
 });
