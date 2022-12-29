3a6bf4a91aeeabef1574d60f9ef519fe2f7e6042
Nathan Burgess <nathans.email.address@gmail.com>
2022-12-28 23:47:53 -0800

    DELETE /users/sessions logs out user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -8,6 +8,17 @@ const mockUser = {
 	password: 'toronto123',
 };
 
+const signInAndLogin = async () => {
+	await request(app).post('/users').send(mockUser);
+	const agent = request.agent(app);
+	await agent.post('/users/sessions').send({
+		email: 'kawhi.leonard@rapptr.com',
+		password: 'toronto123',
+	});
+
+	return agent;
+};
+
 describe('user routes', () => {
 	beforeEach(() => {
 		return setup(pool);
@@ -28,12 +39,17 @@ describe('user routes', () => {
 
 	it('logs in a user', async () => {
 		await request(app).post('/users').send(mockUser);
-		const res = await request(app)
-			.post('/users/sessions')
-			.send({
-				email: 'kawhi.leonard@rapptr.com',
-				password: 'toronto123',
-			});
+		const res = await request(app).post('/users/sessions').send({
+			email: 'kawhi.leonard@rapptr.com',
+			password: 'toronto123',
+		});
 		expect(res.status).toEqual(200);
+		expect(res.body).toEqual({ message: "You're signed in!" });
+	});
+
+	it('signs user out at DELETE /sessions', async () => {
+		const agent = await signInAndLogin();
+		const signOutRes = await agent.delete('/users/sessions');
+		expect(signOutRes.status).toBe(204);
 	});
 });
